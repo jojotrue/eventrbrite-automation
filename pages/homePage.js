bf3f6ace53c54ab1c115c3974c06064f663e54dd
Joanne Trujillo <joannetrujillo8@gmail.com>
2026-02-26 13:21:19 -0700

    Update homePage.js

--- pages/homePage.js
+++ pages/homePage.js
@@ -30,7 +30,7 @@ class HomePage {
 
     // Events Listing Page - Headings & Neighborhood Navigation
     this.eventListingHeading = page.locator('h1');
-    this.neighborhoodTablist = page.getByTestId('filter-section__neighborhood').getByText('Neighborhood');
+    this.neighborhoodTablist = page.getByRole('group').filter({ hasText: 'Neighborhood' });
     this.neighborhoodTabs = this.neighborhoodTablist.getByRole('tab');
 
     // Browse Tabs
